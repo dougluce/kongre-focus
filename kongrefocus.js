11940f656bcbe48ba9a2d19b3518bf5532b02b58
Doug Luce <luce@inbound.aaronsen.com>
2012-01-23 15:08:12 -0800

    Finished code, for Github

---
+++ kongrefocus.js
@@ -0,0 +1,3 @@
+document.getElementById('headerwrap').style.display="none";
+document.getElementById('gamepage_header').style.display="none";
+document.getElementById('subwrap').style.display="none";
